c1cd48e14c5bd90b80686d689c3cbb3c68a1dabc
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-11-11 21:31:37 +0800

    convert to typescript, improvements to text processing and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,100 @@
 import { useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
-function tNow() {
-  const tNow = new Date();
-  let hours = tNow.getHours();
-  if (hours === 0) {
-    hours = "00:";
-  } else if (hours < 10) {
-    hours = `0${hours}:`;
-  } else {
-    hours = `${hours}:`;
-  }
-
-  let minutes = tNow.getMinutes();
-  if (minutes === 0) {
-    minutes = "00:";
-  } else if (minutes < 10) {
-    minutes = `0${minutes}:`;
-  } else {
-    minutes = `${minutes}:`;
-  }
-
-  let seconds = tNow.getSeconds();
-  if (seconds === 0) {
-    seconds = "00";
-  } else if (seconds < 10) {
-    seconds = `0${seconds}`;
-  } else {
-    seconds = `${seconds}`;
-  }
-
-  return hours + minutes + seconds;
+interface Config {
+  angleMin: number;
+  angleMax: number;
+  fermiEnergy: number;
+  isBinding: boolean;
 }
 
-function App() {
+function App(): JSX.Element {
   const [filename, setFilename] = useState("");
-  const [content, setContent] = useState([]);
-  const [data, setData] = useState([]);
-  const [angle, setAngle] = useState([]);
-  const [status, setStatus] = useState([]);
+  const [content, setContent] = useState<string[]>([]);
+  const [data, setData] = useState<number[][]>([]);
+  const [angle, setAngle] = useState<number[]>([]);
+  const [status, setStatus] = useState<string[]>([]);
   const [showCopied, setShowCopied] = useState(false);
   const [isKinetic, setIsKinetic] = useState(false);
-  const [appState, setAppState] = useState({
-    ymin: 0.0,
-    ymax: 0.0,
-    fermiEnergy: 1482,
+  const [config, setConfig] = useState<Config>({
+    angleMin: 0.0,
+    angleMax: 0.0,
+    fermiEnergy: 1482.0,
     isBinding: false,
   });
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      setFilename(file.name);
+      const reader = new FileReader();
+
+      reader.readAsText(file);
+      let content: string[];
+
+      reader.onload = async () => {
+        const text = reader.result?.toString();
+
+        if (text !== undefined) {
+          content = splitByLineBreaks(text);
+        } else {
+          let t = tNow();
+          setStatus([
+            `${t} ❌ Oops! Something went wrong while reading file content.`,
+          ]);
+          console.log("Oops! Something went wrong while reading file content.");
+        }
+
+        setContent(content);
+        setData([]);
+
+        let t = tNow();
+        setStatus([`${t} ✔️ New file '${file.name}' selected.`]);
+
+        let isKineticEnergy = false;
+
+        for (let ii = 0; ii < content.length; ii++) {
+          if (content[ii].split("=")[0].trim() === "Dimension 1 name") {
+            if (content[ii].split("=")[1].trim() === "Kinetic Energy [eV]") {
+              setIsKinetic(true);
+              isKineticEnergy = true;
+            } else {
+              setIsKinetic(false);
+            }
+            break;
+          } else {
+            setIsKinetic(false);
+          }
+        }
+
+        for (let ii = 0; ii < content.length; ii++) {
+          if (content[ii].split("=")[0].trim() === "Dimension 2 scale") {
+            let angle = splitByWhiteSpaces(content[ii].split("=")[1]);
+            angle = angle.filter((x) => x);
+            let angleNum = angle.map((value) => parseFloat(value));
+
+            setAngle(angleNum);
+            setConfig({
+              angleMin: angleNum[0],
+              angleMax: angleNum[angleNum.length - 1],
+              fermiEnergy: 1482,
+              isBinding: false,
+            });
+            break;
+          } else {
+            setAngle([]);
+            setConfig({
+              angleMin: 0.0,
+              angleMax: 0.0,
+              fermiEnergy: 1482,
+              isBinding: isKineticEnergy,
+            });
+          }
+        }
+      };
+    });
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop, maxFiles: 1 });
 
   const ProcessData = () => {
     if (!filename) {
@@ -55,20 +103,20 @@ function App() {
       return;
     }
 
-    let energy = [],
-      intensity = [],
-      energyDimSize = 0,
-      angleDimSize = 0,
-      data = [],
-      dataStart = 0;
+    let energy = [];
+    let intensity = [];
+    let energyDimSize = 0;
+    let angleDimSize = 0;
+    let data = [];
+    let dataStart = 0;
 
     for (let ii = 0; ii < content.length; ii++) {
-      if (content[ii].split("=")[0] === "Dimension 1 size") {
+      if (content[ii].split("=")[0].trim() === "Dimension 1 size") {
         energyDimSize = parseInt(content[ii].split("=")[1]);
         // console.log("Dimension 1 size: ", energyDimSize);
       }
 
-      if (content[ii].split("=")[0] === "Dimension 2 size") {
+      if (content[ii].split("=")[0].trim() === "Dimension 2 size") {
         angleDimSize = parseInt(content[ii].split("=")[1]);
         // console.log("Dimension 2 size: ", angleDimSize);
       }
@@ -79,10 +127,10 @@ function App() {
       }
     }
 
-    let angleStart = 0,
-      angleEnd = angle.length - 1,
-      yMin = parseFloat(appState.ymin),
-      yMax = parseFloat(appState.ymax);
+    let angleStart = 0;
+    let angleEnd = angle.length - 1;
+    let yMin = config.angleMin;
+    let yMax = config.angleMax;
 
     if (yMin !== angle[0]) {
       let closestPoint = angle.map((value) => Math.abs(value - yMin));
@@ -97,16 +145,27 @@ function App() {
     }
 
     for (let ii = dataStart; ii < dataStart + energyDimSize; ii++) {
-      let temp = [],
-        tempEnergy = 0,
-        tempIntensity = 0;
+      let temp: string[] = [];
+      let tempEnergy = 0;
+      let tempIntensity = 0;
 
-      temp = content[ii].split(" ");
+      temp = splitByWhiteSpaces(content[ii]);
       temp = temp.filter((x) => x);
-      tempEnergy = parseFloat(temp.shift());
 
-      if (appState.isBinding) {
-        tempEnergy = parseFloat(appState.fermiEnergy) - tempEnergy;
+      let tempEnergyStr = temp.shift();
+      if (tempEnergyStr !== undefined) {
+        tempEnergy = parseFloat(tempEnergyStr);
+      } else {
+        let t = tNow();
+        setStatus([
+          ...status,
+          `${t} ❌ Error occur while parsing energy values.`,
+        ]);
+        console.log("Error occur while parsing energy values.");
+      }
+
+      if (config.isBinding) {
+        tempEnergy = config.fermiEnergy - tempEnergy;
       }
 
       for (let jj = angleStart; jj <= angleEnd; jj++) {
@@ -153,77 +212,15 @@ function App() {
     }
   };
 
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file) => {
-      setFilename(file.name);
-      const reader = new FileReader();
-
-      reader.readAsText(file);
-      reader.onload = async () => {
-        const text = reader.result;
-        const content = text.split("\n");
-
-        setContent(content);
-        setData([]);
-
-        let t = tNow();
-        setStatus([`${t} ✔️ New file '${file.name}' selected.`]);
-
-        let isKineticEnergy = false;
-
-        for (let ii = 0; ii < content.length; ii++) {
-          if (content[ii].split("=")[0] === "Dimension 1 name") {
-            if (content[ii].split("=")[1].trim() === "Kinetic Energy [eV]") {
-              setIsKinetic(true);
-              isKineticEnergy = true;
-            } else {
-              setIsKinetic(false);
-            }
-            break;
-          } else {
-            setIsKinetic(false);
-          }
-        }
-
-        for (let ii = 0; ii < content.length; ii++) {
-          if (content[ii].split("=")[0] === "Dimension 2 scale") {
-            let angle = content[ii].split("=")[1].split(" ");
-            angle = angle.filter((x) => x);
-            angle = angle.map((value) => parseFloat(value));
-
-            setAngle(angle);
-            setAppState({
-              ymin: angle[0],
-              ymax: angle[angle.length - 1],
-              fermiEnergy: 1482,
-              isBinding: false,
-            });
-            break;
-          } else {
-            setAngle([]);
-            setAppState({
-              ymin: 0.0,
-              ymax: 0.0,
-              fermiEnergy: 1482,
-              isBinding: isKineticEnergy,
-            });
-          }
-        }
-      };
-    });
-  }, []);
-
-  const { getRootProps, getInputProps } = useDropzone({ onDrop, maxFiles: 1 });
-
   const DownloadPlaintext = () => {
-    let outFilename = null;
+    let outFilename = "";
     if (
       filename.slice(filename.length - 4, filename.length).toLowerCase() ===
       ".txt"
     ) {
-      outFilename = filename.slice(0, filename.length - 4);
+      outFilename = filename.slice(0, filename.length - 4) + ".x_y";
     } else {
-      outFilename = filename;
+      outFilename = filename + ".x_y";
     }
 
     if (data.length) {
@@ -231,16 +228,14 @@ function App() {
 
       for (let ii = 0; ii < data.length; ii++) {
         downloadContent = downloadContent.concat(
-          `${parseFloat(data[ii][0])}\t${parseFloat(
-            data[ii][1]
-          ).toExponential()}\r\n`
+          `${data[ii][0]}\t${data[ii][1].toExponential()}\r\n`
         );
       }
 
       const element = document.createElement("a");
       const file = new Blob([downloadContent], { type: "text/plain" });
       element.href = URL.createObjectURL(file);
-      element.download = outFilename + ".x_y";
+      element.download = outFilename;
       document.body.appendChild(element); // Required for this to work in FireFox
       element.click();
     } else {
@@ -259,9 +254,7 @@ function App() {
 
       for (let ii = 0; ii < data.length; ii++) {
         clipboardContent = clipboardContent.concat(
-          `${parseFloat(data[ii][0])}\t${parseFloat(
-            data[ii][1]
-          ).toExponential()}\r\n`
+          `${data[ii][0]}\t${data[ii][1].toExponential()}\r\n`
         );
       }
       navigator.clipboard.writeText(clipboardContent);
@@ -273,12 +266,13 @@ function App() {
     }
   };
 
-  const HandleChange = (e) => {
+  const HandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.target;
     const name = target.name;
     const value = target.type === "checkbox" ? target.checked : target.value;
 
-    setAppState({ ...appState, [name]: value });
+    setConfig({ ...config, [name]: value });
+    setData([]);
   };
 
   return (
@@ -324,17 +318,17 @@ function App() {
         </div>
 
         <form className="form">
-          {angle.length ? (
+          {angle.length > 0 && (
             <>
               <p>Angular integration limits:</p>
               <p>
                 θ<sub>min</sub> = &nbsp;
                 <input
                   type="text"
-                  id="ymin"
-                  name="ymin"
-                  placeholder={0.0}
-                  value={appState.ymin}
+                  id="angleMin"
+                  name="angleMin"
+                  placeholder={(0.0).toString()}
+                  value={config.angleMin}
                   onChange={HandleChange}
                   style={{ width: "120px" }}
                 />
@@ -343,54 +337,54 @@ function App() {
                 θ<sub>max</sub> = &nbsp;
                 <input
                   type="text"
-                  id="ymax"
-                  name="ymax"
-                  placeholder={0.0}
-                  value={appState.ymax}
+                  id="angleMax"
+                  name="angleMax"
+                  placeholder={(0.0).toString()}
+                  value={config.angleMax}
                   onChange={HandleChange}
                   style={{ width: "120px" }}
                 />
               </p>
             </>
-          ) : null}
+          )}
 
-          {isKinetic ? (
+          {isKinetic && (
             <p>
               <input
                 type="checkbox"
                 style={{ width: "25px" }}
                 id="isBinding"
                 name="isBinding"
-                checked={appState.isBinding}
+                checked={config.isBinding}
                 onChange={HandleChange}
               />
               Convert to binding energy (E<sub>bin</sub> = E<sub>F</sub> - E
               <sub>kin</sub>)
             </p>
-          ) : null}
+          )}
 
-          {appState.isBinding ? (
+          {config.isBinding && (
             <p>
               Fermi energy (hν - W<sub>φ</sub>) =&nbsp;
               <input
                 type="text"
                 id="fermiEnergy"
                 name="fermiEnergy"
-                placeholder={1482.0}
-                value={appState.fermiEnergy}
+                placeholder={(1482.0).toString()}
+                value={config.fermiEnergy}
                 onChange={HandleChange}
               />
             </p>
-          ) : null}
+          )}
         </form>
 
-        {filename ? (
+        {filename && (
           <button onClick={ProcessData} className="btn">
             Convert
           </button>
-        ) : null}
+        )}
 
-        {data.length ? (
+        {data.length > 0 && (
           <>
             <button className="btn" onClick={DownloadPlaintext}>
               Save
@@ -399,19 +393,20 @@ function App() {
               {showCopied ? "Copied" : "Copy"}
             </button>
           </>
-        ) : null}
+        )}
 
         <br />
         <br />
 
-        {status.length ? (
+        {status.length > 0 && (
           <>
             {status.map((item, key) => (
               <p key={key}>{item}</p>
             ))}
           </>
-        ) : null}
-        {data.length ? (
+        )}
+
+        {data.length > 0 && (
           <table>
             <tbody>
               <tr>
@@ -421,16 +416,16 @@ function App() {
               {data.map((value, key) => (
                 <tr key={key}>
                   <td>
-                    <code>{parseFloat(value[0])}</code>
+                    <code>{value[0]}</code>
                   </td>
                   <td>
-                    <code>{parseFloat(value[1]).toExponential()}</code>
+                    <code>{value[1].toExponential()}</code>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
-        ) : null}
+        )}
         <br />
         <br />
       </div>
@@ -441,5 +436,57 @@ function App() {
     </div>
   );
 }
+
+const tNow = () => {
+  const tNow = new Date();
+  let hours = tNow.getHours();
+  let hrsStr: string;
+  if (hours === 0) {
+    hrsStr = "00:";
+  } else if (hours < 10) {
+    hrsStr = `0${hours}:`;
+  } else {
+    hrsStr = `${hours}:`;
+  }
+
+  let minutes = tNow.getMinutes();
+  let minStr: string;
+  if (minutes === 0) {
+    minStr = "00:";
+  } else if (minutes < 10) {
+    minStr = `0${minutes}:`;
+  } else {
+    minStr = `${minutes}:`;
+  }
+
+  let seconds = tNow.getSeconds();
+  let secStr: string;
+  if (seconds === 0) {
+    secStr = "00";
+  } else if (seconds < 10) {
+    secStr = `0${seconds}`;
+  } else {
+    secStr = `${seconds}`;
+  }
+
+  return hrsStr + minStr + secStr;
+};
+
+const splitByLineBreaks = (content: string) => {
+  return content
+    .replace(/\r/g, "\n") // replace carriage return `\r` to `\n`
+    .replace(/[\n]+/g, "\n") // replace multiple `\n` by single `\n`
+    .replace(/^\n/, "") // trim any new line character in the beginning
+    .replace(/\n$/, "") // trim any new line character at the end
+    .split("\n");
+};
+
+const splitByWhiteSpaces = (line: string) => {
+  return line
+    .replace(/\t/g, " ") // replace tab character by space
+    .replace(/[\s]+/g, " ") // replace multiple space characters by single one
+    .trim() // trim any beginning or trailing spaces
+    .split(/\s/);
+};
 
 export default App;
